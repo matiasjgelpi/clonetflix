0df95da05582b17e95affbf136f1c5592db1c4d7
matiasjgelpi <matiasjgelpi@gmail.com>
2022-03-12 18:08:27 -0300

    firsts components styled

--- src/Row.js
+++ src/Row.js
@@ -2,9 +2,9 @@ import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import "./Row.css";
 
-const base_url = "https://image.tmdb.org/t/p/original/";
+const base_url = "https://image.tmdb.org/t/p/original";
 
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
@@ -24,8 +24,11 @@ function Row({ title, fetchUrl }) {
       <div className="row_posters">
         {movies.map((movie) => (
           <img
-            className="row_poster"
-            src={`${base_url}${movie.poster_path}`}
+            key={movie.id}
+            className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
             alt={movie.name}
           />
         ))}
